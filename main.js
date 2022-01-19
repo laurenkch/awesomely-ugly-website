c52286eb4f5d6b4ab70a032cdb703f6f4958d6f1
laurenkch <lkoch879@gmail.com>
2022-01-19 10:37:52 -0500

    click arrow

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ picture.addEventListener('click', function() {
     if(this.src == "https://lifewhack.me/wp-content/uploads/2014/05/1-3.jpg") {
     this.src = "https://c.tenor.com/Ok5CTp9-ykkAAAAC/ara%C3%B1a-spider.gif" 
     this.style.width = `700px`
+    this.style.heigth = "388.5px"
 } else {
     this.src = "https://lifewhack.me/wp-content/uploads/2014/05/1-3.jpg"
 };
